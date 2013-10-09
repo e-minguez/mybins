51ab636a01b592e8af31a7203f7fbdfb7726f9fe
eminguez <eminguez@redhat.com>
2013-10-09 09:32:24 +0200

    Added random.sh to log random numbers with random severity each x seconds (0<x<15)

--- Other/random.sh
+++ Other/random.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 SEVERITY_LIST="alert crit debug emerg err info notice warning"
+OLDIFS=$IFS
+IFS=' '
+sev=($SEVERITY_LIST)
+num_sev=${#sev[*]}
+IFS=$OLDIFS
 while true
 do
 	RAND=$RANDOM
-	OLDIFS=$IFS
-	IFS=' '
-	sev=($SEVERITY_LIST)
-	num_sev=${#sev[*]}
-	IFS=$OLDIFS
 	SEVERITY=${sev[$((RAND%num_sev))]}
-	logger -p user.$SEVERITY $RAND
-	#echo $RAND - $SEVERITY
+	#logger -p user.$SEVERITY $RAND
+	echo $RAND - $SEVERITY
 	sleep $(( RAND %= 15 ))
 done
